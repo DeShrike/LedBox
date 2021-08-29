58ff1888ca8c99b598f8d449efaa6a85ae6e2428
Shrike <de_shrike@hotmail.com>
2021-08-29 10:57:43 +0200

    Starting conversion to Vue3

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,3 +1,27 @@
+const VueApp = {
+    data() {
+        return {
+            counter: 42,
+            msg: ""
+        }
+    },
+    mounted() {
+        setInterval(() => {
+            this.counter++
+        }, 1000)
+    },
+    methods: {
+        menuClick(action) {
+            this.msg = action;
+        },
+        arrowClick(action) {
+            this.msg = action;
+        }
+    }
+};
+
+Vue.createApp(VueApp).mount('#ledbox-vue');
+/*
 var LedBox = (function() {
     "use strict";
 
@@ -5,37 +29,6 @@ var LedBox = (function() {
     var connected = false;
     var server;
 
-    /*
-    var Ajax = {
-        request : function (url, method, data, success, failure) {
-            // Here
-            var xhr = window.ActiveX ? new ActiveXObject("Microsoft.XMLHTTP") : new XMLHttpRequest();
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState === 4 && xhr.status === 200) {
-                    // the request is complete, parse data and call callback
-                    // var response = JSON.parse(xhr.responseText);
-                    // success(response);
-                    success(xhr.responseText);
-                }
-                else if (xhr.readyState === 4) { // something went wrong but complete
-                    failure();
-                }
-            };
-
-            xhr.open(method, url, true);
-            if (method === "POST") {
-                this.xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-                // this.xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-                this.xhr.setRequestHeader('Content-Type', 'text/json');
-                this.xhr.send(data);
-            }
-            else {
-                this.xhr.send();
-            }
-        }
-    };
-    */
-    
     function addInfo(msg)
     {
         var html = $(".info-block").html();
@@ -46,20 +39,20 @@ var LedBox = (function() {
     function init() {
 
         addInfo("Init");
-    	$(".special-button").on("click", specialButton);
-    	$(".arrow-button").on("click", arrowButton);
-    	$(".action-button").on("click", actionButton);
+        $(".special-button").on("click", specialButton);
+        $(".arrow-button").on("click", arrowButton);
+        $(".action-button").on("click", actionButton);
 
         $(".arrow-button").hide();
 
-    	server = "http://" + document.domain + ":" + location.port;
+        server = "http://" + document.domain + ":" + location.port;
         addInfo("Connecting");
-	    socket = io.connect(server);
+        socket = io.connect(server);
 
         // connect error disconnect reconnect reconnect_attempt reconnecting reconnect_error
 
-    	socket.on("Connected", function(msg) {
-			console.log("Connected", msg);
+        socket.on("Connected", function(msg) {
+            console.log("Connected", msg);
             addInfo("Connected");
             connected = true;
         }).on("connect", function(msg) {
@@ -108,62 +101,43 @@ var LedBox = (function() {
         // setTimeout(2000, startWithoutSockets);
     }
 
-    /*
-    function startWithoutSockets() {
-        if (connected) {
-            return;
-        }
+    // // function startWithoutSockets() {
+    // //     if (connected) {
+    // //         return;
+    // //     }
 
-        Ajax.request("/ledbox/state", "GET", null, function(resp) {
-            addInfo(resp);
-        },
-        function() {
+    // //     Ajax.request("/ledbox/state", "GET", null, function(resp) {
+    // //         addInfo(resp);
+    // //     },
+    // //     function() {
 
-        });
-    }
-    */
+    // //     });
+    // // }
 
     function specialButton(e) {
         var action = $(this).data("action");
-    	console.log("Special", action);
-    	socket.emit("SpecialEvent", { data: action });
+        console.log("Special", action);
+        socket.emit("SpecialEvent", { data: action });
     }
 
     function arrowButton(e) {
-    	var arrow = $(this).data("arrow");
-    	console.log("Arrow", arrow);
-    	socket.emit("ArrowEvent", { data: arrow });
+        var arrow = $(this).data("arrow");
+        console.log("Arrow", arrow);
+        socket.emit("ArrowEvent", { data: arrow });
     }
 
     function actionButton(e) {
-    	var action = $(this).data("action");
-    	console.log("Action", action);
-    	socket.emit("ActionEvent", { data: action });
+        var action = $(this).data("action");
+        console.log("Action", action);
+        socket.emit("ActionEvent", { data: action });
     }
 
     function showCurrentAction(currentAction) {
         $("#current-action").html(currentAction);
     }
 
-    /*function ajax(url, data, cb) {
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    if (xhr.responseType === "json") {
-                        var resp = JSON.parse(xhr.responseText);
-                        cb(resp);
-                    }
-                }
-            }
-        };
-
-        xhr.open("POST", server + url);
-        xhr.responseType = "json";
-        xhr.send(data);
-    }*/
-
     return {
         init: init,
     };
 })();
+*/
